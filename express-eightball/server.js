c8bffa7d4aa7fbafe0d39e012a2bed1a52a1efea
Ibrakhim Abdurachmanov <bleach_fanart@hotmail.com>
2018-09-03 16:46:16 +0200

    fail

--- express-eightball/server.js
+++ express-eightball/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const eightball = require('8ball');
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
 });
@@ -10,3 +10,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+var item;
+var answers = [
+  'its certain',
+  'its decidedly',
+  'very doubtful',
+   'dont count on it'
+];
+
+var questions = [];
+// Redirect to list
+app.get('/', (req, res) => {
+   res.redirect('/home')
+})
+
+// Show the ask form
+app.get('/', (req, res) => {
+   res.render('add.ejs', {})
+})
+
+// Show the home form
+app.get('/home', (req, res) => {
+   res.render('home.ejs', {})
+})
+
+
+app.post('/answer', function (req, res) {
+  var question = req.body.name;
+  questions.push(question);
+  item = answers[Math.floor(Math.random()*answers.length)];
+  res.json({answer:item})
+
+    res.render('home.ejs',{})
+})
